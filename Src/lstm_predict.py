383399f93c6739f483ae5928cae768408a6ed5bf
Kemal Guddeta <kguddeta@yahoo.com>
2018-04-22 02:09:50 -0500

    Update lstm_predict.py

--- Src/lstm_predict.py
+++ Src/lstm_predict.py
@@ -1,1 +1,129 @@
+#@Kemal Guddeta
+#@Machine Learning and Pattern Recognition
+# LSTM for sp500 stock price prediction
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation,Dropout
+from keras.layers import LSTM,LeakyReLU
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+from numpy import array
 
+shape = [1, 1, 1]
+neurons = [64, 64, 32, 1]
+dropout = 0.3
+decay = 0.5
+epochs = 10
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+	
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset
+dataframe = read_csv('yahoo_close_price.csv', usecols=[0], engine='python')
+dataset = dataframe.values
+dataset = dataset.astype('float64')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.80)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+def build_model(shape, neurons, dropout, decay):
+    model = Sequential()
+    model.add(LSTM(neurons[0], input_shape=(shape[0], shape[1]), return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(neurons[1], input_shape=(shape[0], shape[1]), return_sequences=False))
+    model.add(Dropout(dropout))
+    model.add(Dense(neurons[2],kernel_initializer="uniform",activation='relu'))
+    model.add(Dense(neurons[3],kernel_initializer="uniform",activation='linear'))
+    adam = keras.optimizers.Adam(decay=decay)
+    model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
+
+#Compile the model	
+model = build_model(shape, neurons, dropout, decay)
+
+#Fit the model
+model.fit(trainX,trainY,batch_size=5,epochs=epochs,validation_split=0.2,verbose=1)
+
+#Define function to get test score
+def model_score(model, X_train, y_train, X_test, y_test):
+    trainScore = model.evaluate(X_train, y_train, verbose=0)
+    print('Train Score: %.5f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
+
+    testScore = model.evaluate(X_test, y_test, verbose=0)
+    print('Test Score: %.5f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
+    return trainScore[0], testScore[0]
+
+#Get test scores
+model_score(model,trainX, trainY, testX, testY)
+
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset),'g', label = 'original dataset')
+plt.plot(trainPredictPlot,'r', label = 'training set')
+plt.plot(testPredictPlot,'c', label = 'predicted stock price/test set')
+plt.legend(loc = 'lower right')
+plt.xlabel('Time in Days')
+plt.ylabel('S&P500 Stock price ')
+plt.show()
+
+
+# PREDICT FUTURE VALUES
+last_val = testPredict[-1]
+last_val_scaled = last_val/last_val
+next_val = model.predict(numpy.reshape(last_val_scaled, (1,1,1)))
+print ("Last Day Value:", numpy.asscalar(last_val))
+print ("Next Day Value:", numpy.asscalar(last_val*next_val))
+print (numpy.append(last_val, next_val))
